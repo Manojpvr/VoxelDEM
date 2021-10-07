518e35438a66182c89eb748364419e214785ac77
Manoj <42966051+Manojpvr@users.noreply.github.com>
2021-10-07 22:47:16 +0530

    Update ms_coffee.py

--- ms_coffee.py
+++ ms_coffee.py
@@ -4,12 +4,18 @@ Created on Wed Jul 21 15:13:07 2021
 
 @author: pvrma
 """
+"""
+This is the script for voxelising the ellipsoidal super quadric particle
+data. 
+"""
+
 import voxelization
 zmax = -0.003 #removes particles above this height, to avoid heaping effect
-
-par_data = voxelization.import_sq_data('sq_ellipsoid_dem_out.csv', zmax)
-
+par_data = voxelization.import_sq_data('sq_ellipsoid_dem_out.csv', zmax)#importing dem output data
 rmin = 0.0006 #used to calculate the normalized_voxel_size
-
 normalized_voxel_size = 0.1
-voxel = voxelization.sq_vox(par_data, rmin, [-0.0125,0.0125,-0.0125,0.0125,-0.05,-0.001], normalized_voxel_size, 'ellipsoid_sq')
+bounding_box_dimensions = [-0.0125,0.0125,-0.0125,0.0125,-0.05,-0.001]#[xmin,xmax,ymin,ymax,zmin,zmax]
+"""
+bounding box dimensions need to be set in such a way that no particle pritudes outside it.
+"""
+voxel = voxelization.sq_vox(par_data, rmin, bounding_box_dimensions, normalized_voxel_size, 'ellipsoid_sq')#voxelizing the particle data
