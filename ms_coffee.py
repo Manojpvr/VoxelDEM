1483e8241c544a91cbe3572b959a7c3b7733ab79
Manoj <42966051+Manojpvr@users.noreply.github.com>
2022-09-01 22:39:43 +0530

    Update ms_coffee.py

--- ms_coffee.py
+++ ms_coffee.py
@@ -12,7 +12,6 @@ data.
 import voxelization
 zmax = -0.003 #removes particles above this height, to avoid heaping effect
 par_data = voxelization.import_ms_data('ms_coffee_dem_out.csv','coffee_sphere_data.csv', zmax)#importing dem output data
-print
 rmin = 0.0006 #used to calculate the normalized_voxel_size
 normalized_voxel_size = 0.1
 bounding_box_dimensions = [-0.0125,0.0125,-0.0125,0.0125,-0.05,-0.001]#[xmin,xmax,ymin,ymax,zmin,zmax]
